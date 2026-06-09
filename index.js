aa10726db2a04b54c816725d0ec156b448bfc44e
erikalfonsocGIT <erikalfonsoc@gmail.com>
2026-06-09 17:14:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+JavaScript
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+app.use('/', createProxyMiddleware({
+    target: 'https://generativelanguage.googleapis.com',
+    changeOrigin: true,
+    logLevel: 'debug'
+}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Proxy activo en puerto ${PORT}`));
